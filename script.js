4e48c1713702b4294aa1458e4e3424630ba38e7f
WellingtonFernando2 <Wellington1.00043@gmail.com>
2024-05-11 15:35:03 -0300

    atualizações

--- script.js
+++ script.js
@@ -7,16 +7,28 @@ menu.addEventListener('click', () => {
 })
 
 
-const openModalButton = document.querySelector("#open-modal");
-const closeModalButton = document.querySelector("#close-modal");
-const modal = document.querySelector("#modal");
-const fade = document.querySelector("#fade");
+// Get the modal
+var modal = document.getElementById("myModal");
 
-const toggleModal = () => {
-  modal.classList.toggle("hide");
-  fade.classList.toggle("hide");
-};
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-[openModalButton, closeModalButton, fade].forEach((el) => {
-  el.addEventListener("click", () => toggleModal());
-});
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
