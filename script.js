2cb6497e4877be71869bc654d8aeb429c22313b0
WellingtonFernando2 <Wellington1.00043@gmail.com>
2024-06-02 19:39:36 -0300

    atualizações

--- script.js
+++ script.js
@@ -64,4 +64,57 @@ span2.onclick = function() {
         modal3.style.display = "none";
       }
     };
+
+  var modal4 = document.getElementById("myModal4");
+  var btn4 = document.getElementById("myBtn4");
+  var span4 = document.getElementsByClassName("close4")[0];
   
+  btn4.onclick = function() {
+    modal4.style.display = "block";
+  };
+  
+  span4.onclick = function() {
+    modal4.style.display = "none"
+  };
+  
+    window.onclick = function(event) {
+      if (event.target == modal4) {
+        modal4.style.display = "none";
+      }
+    };
+  
+    var modal5 = document.getElementById("myModal5");
+    var btn5 = document.getElementById("myBtn5");
+    var span5 = document.getElementsByClassName("close5")[0];
+    
+    btn5.onclick = function() {
+      modal5.style.display = "block";
+    };
+    
+    span5.onclick = function() {
+      modal5.style.display = "none"
+    };
+    
+      window.onclick = function(event) {
+        if (event.target == modal5) {
+          modal5.style.display = "none";
+        }
+      };
+
+      var modal6 = document.getElementById("myModal6");
+      var btn6 = document.getElementById("myBtn6");
+      var span6 = document.getElementsByClassName("close6")[0];
+      
+      btn6.onclick = function() {
+        modal6.style.display = "block";
+      };
+      
+      span6.onclick = function() {
+        modal6.style.display = "none"
+      };
+      
+        window.onclick = function(event) {
+          if (event.target == modal6) {
+            modal6.style.display = "none";
+          }
+        };
